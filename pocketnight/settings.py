bff5ae76b5f86ec867070dea4bb027a7bf274aab
Nicolas Saraceno <nicolas@uakami.com>
2014-10-06 10:05:18 -0300

    adffs

--- pocketnight/settings.py
+++ pocketnight/settings.py
@@ -57,11 +57,13 @@ WSGI_APPLICATION = 'pocketnight.wsgi.application'
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+  }
 }
+
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
